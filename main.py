bf70e7f653377673c40e2a28f4d3f7603f77a549
EliTorn <103903954+EliTorn@users.noreply.github.com>
2024-03-04 21:19:53 +0200

    Add files via upload
    add main files

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+
+def main():
+    st.title("Image Uploader")
+
+    # Display a file uploader widget
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    # If an image is uploaded, display it
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+        from PIL import Image
+        from PIL.ExifTags import TAGS
+
+        def get_exif_data(image_path):
+            try:
+                image = Image.open(image_path)
+                exif_data = image._getexif()
+                if exif_data is not None:
+                    for tag, value in exif_data.items():
+                        tag_name = TAGS.get(tag, tag)
+                        print(f"{tag_name}: {value}")
+                        st.write(f"{tag_name}: {value}")
+                else:
+                    print("No EXIF data found.")
+                    st.write("No EXIF data found.")
+            except Exception as e:
+                print(f"Error: {e}")
+
+        get_exif_data('b.jpg')
+
+
+if __name__ == "__main__":
+    main()
